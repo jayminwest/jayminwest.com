3fc017e2c25916f6be04f64f1f8e24c732c1dc8f
Jaymin West <jayminwest@gmail.com>
2024-11-06 13:53:13 -0700

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      strict: true,
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  build: {
+    sourcemap: true,
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 });
